082caf2f36f67e7ca36f1eff5c55ebaf84de5b15
Roshan <lalasarthakr@gmail.com>
2023-10-03 17:55:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,206 @@
+/* <div class="foodcards">
+                <div class="img">
+
+                </div>
+                <div class="foodfooter">
+                    <div class="footerleft">
+                        <p>veg</p>
+                        <h4>Russioan salad</h4>
+                        <p>40 min</p>
+                    </div>
+                    <div class="footerright">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                            <rect width="22" height="22" fill="white"/>
+                            <path d="M15.3927 3.57866C16.5976 3.64527 17.7273 4.18541 18.5357 5.08136C19.344 5.97731 19.7656 7.15645 19.7083 8.36183C19.7083 11.1778 17.2773 12.9076 14.9444 14.982C12.6417 17.0381 11.4015 18.1619 11 18.4213C10.5627 18.1381 9.03557 16.7502 7.05557 14.982C4.71257 12.8993 2.29166 11.1531 2.29166 8.36183C2.23443 7.15645 2.65593 5.97731 3.46431 5.08136C4.27269 4.18541 5.40243 3.64527 6.60732 3.57866C7.27479 3.55843 7.93608 3.71175 8.52656 4.02361C9.11703 4.33548 9.61648 4.79523 9.97607 5.35791C10.7461 6.435 10.8744 6.97399 11.0027 6.97399C11.1311 6.97399 11.2576 6.435 12.0202 5.35516C12.3778 4.78987 12.8774 4.3283 13.4692 4.01657C14.061 3.70483 14.7242 3.55383 15.3927 3.57866ZM15.3927 1.74533C14.5603 1.71868 13.7326 1.88015 12.9712 2.21773C12.2099 2.55531 11.5345 3.0603 10.9954 3.69508C10.4567 3.06214 9.78276 2.55833 9.02324 2.22085C8.26372 1.88338 7.43808 1.72087 6.60732 1.74533C4.91595 1.81147 3.31959 2.54465 2.1673 3.78454C1.01501 5.02443 0.400567 6.67015 0.458322 8.36183C0.458322 11.671 2.79582 13.7032 5.05541 15.6677C5.31482 15.8932 5.57699 16.1205 5.83732 16.3524L6.77874 17.1939C7.80544 18.1709 8.88182 19.0943 10.0036 19.9604C10.3004 20.1526 10.6464 20.2548 11 20.2548C11.3536 20.2548 11.6996 20.1526 11.9964 19.9604C13.1539 19.0678 14.2634 18.1147 15.3202 17.105L16.1654 16.3497C16.434 16.1113 16.7062 15.8739 16.9767 15.6402C19.1162 13.7839 21.5417 11.6802 21.5417 8.36183C21.5994 6.67015 20.985 5.02443 19.8327 3.78454C18.6804 2.54465 17.084 1.81147 15.3927 1.74533Z" fill="#FF0000"/>
+                          </svg>
+                    </div>
+                </div>
+                
+            </div>*/
+            /* {
+        "name": "Veggie Delight",
+        "imageSrc": "https://source.unsplash.com/random?veggies",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    }*/
+    let arr=[
+        {
+            "name": "Veggie Delight",
+            "imageSrc": "https://source.unsplash.com/random?veggies",
+            "time": "30 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.2
+        },
+        {
+            "name": "Chicken Grill",
+            "imageSrc": "https://source.unsplash.com/random?chicken",
+            "time": "45 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.5
+        },
+        {
+            "name": "Cheese Pizza",
+            "imageSrc": "https://source.unsplash.com/random?pizza",
+            "time": "40 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.1
+        },
+        {
+            "name": "Steak",
+            "imageSrc": "https://source.unsplash.com/random?steak",
+            "time": "60 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.7
+        },
+        {
+            "name": "Grilled Salmon",
+            "imageSrc": "https://source.unsplash.com/random?salmon",
+            "time": "50 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.6
+        },
+        {
+            "name": "Tomato Pasta",
+            "imageSrc": "https://source.unsplash.com/random?pasta",
+            "time": "35 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.0
+        },
+        {
+            "name": "Vegan Salad",
+            "imageSrc": "https://source.unsplash.com/random?salad",
+            "time": "20 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 3.9
+        },
+        {
+            "name": "Fried Chicken",
+            "imageSrc": "https://source.unsplash.com/random?friedChicken",
+            "time": "55 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.3
+        },
+        {
+            "name": "Mushroom Risotto",
+            "imageSrc": "https://source.unsplash.com/random?risotto",
+            "time": "45 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.5
+        },
+        {
+            "name": "Burger",
+            "imageSrc": "https://source.unsplash.com/random?burger",
+            "time": "30 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.2
+        },
+        {
+            "name": "Paneer Tikka",
+            "imageSrc": "https://source.unsplash.com/random?paneerTikka",
+            "time": "40 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.4
+        },
+        {
+            "name": "BBQ Ribs",
+            "imageSrc": "https://source.unsplash.com/random?ribs",
+            "time": "70 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.6
+        },
+        {
+            "name": "Caesar Salad",
+            "imageSrc": "https://source.unsplash.com/random?caesarSalad",
+            "time": "25 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 3.8
+        },
+        {
+            "name": "Fish Tacos",
+            "imageSrc": "https://source.unsplash.com/random?fishTacos",
+            "time": "35 min",
+            "type": "non-veg",
+            "isLiked": false,
+            "rating": 4.3
+        },
+        {
+            "name": "Chocolate Cake",
+            "imageSrc": "https://source.unsplash.com/random?chocolateCake",
+            "time": "90 min",
+            "type": "veg",
+            "isLiked": false,
+            "rating": 4.9
+        }
+    ]
+   
+    
+    const foodsection=document.getElementById("foodsection");
+   myrender();
+    const allrec=document.getElementById("allrec");
+    const veg=document.getElementById("veg");
+    const nonveg=document.getElementById("nonveg");
+    allrec.addEventListener("click",allrender(arr));
+   // veg.addEventListener("click",vegrender);
+   // nonveg.addEventListener("click",nonvegrender);
+   function allrender(){
+    myrender(arr);
+   }
+
+
+  
+
+
+
+
+
+
+
+
+    function myrender(foods=arr){
+        let section= document.getElementById("foodsection");
+        section.innerHTML="";
+       foods.forEach((x)=>{
+        
+       const newnode=document.createElement("div");
+        let img=x.imageSrc;
+       let name=x.name;
+       let time=x.time;
+       let type=x.type;
+       newnode.innerHTML=`<div class="foodcards">
+       <div class="immg">
+           <img src=${img}>
+       </div>
+       <div class="foodfooter">
+           <div class="footerleft">
+               <p>${type}</p>
+               <h4>${name}</h4>
+               <p>${time}</p>
+           </div>
+           <div class="footerright">
+               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                   <rect width="22" height="22" fill="white"/>
+                   <path d="M15.3927 3.57866C16.5976 3.64527 17.7273 4.18541 18.5357 5.08136C19.344 5.97731 19.7656 7.15645 19.7083 8.36183C19.7083 11.1778 17.2773 12.9076 14.9444 14.982C12.6417 17.0381 11.4015 18.1619 11 18.4213C10.5627 18.1381 9.03557 16.7502 7.05557 14.982C4.71257 12.8993 2.29166 11.1531 2.29166 8.36183C2.23443 7.15645 2.65593 5.97731 3.46431 5.08136C4.27269 4.18541 5.40243 3.64527 6.60732 3.57866C7.27479 3.55843 7.93608 3.71175 8.52656 4.02361C9.11703 4.33548 9.61648 4.79523 9.97607 5.35791C10.7461 6.435 10.8744 6.97399 11.0027 6.97399C11.1311 6.97399 11.2576 6.435 12.0202 5.35516C12.3778 4.78987 12.8774 4.3283 13.4692 4.01657C14.061 3.70483 14.7242 3.55383 15.3927 3.57866ZM15.3927 1.74533C14.5603 1.71868 13.7326 1.88015 12.9712 2.21773C12.2099 2.55531 11.5345 3.0603 10.9954 3.69508C10.4567 3.06214 9.78276 2.55833 9.02324 2.22085C8.26372 1.88338 7.43808 1.72087 6.60732 1.74533C4.91595 1.81147 3.31959 2.54465 2.1673 3.78454C1.01501 5.02443 0.400567 6.67015 0.458322 8.36183C0.458322 11.671 2.79582 13.7032 5.05541 15.6677C5.31482 15.8932 5.57699 16.1205 5.83732 16.3524L6.77874 17.1939C7.80544 18.1709 8.88182 19.0943 10.0036 19.9604C10.3004 20.1526 10.6464 20.2548 11 20.2548C11.3536 20.2548 11.6996 20.1526 11.9964 19.9604C13.1539 19.0678 14.2634 18.1147 15.3202 17.105L16.1654 16.3497C16.434 16.1113 16.7062 15.8739 16.9767 15.6402C19.1162 13.7839 21.5417 11.6802 21.5417 8.36183C21.5994 6.67015 20.985 5.02443 19.8327 3.78454C18.6804 2.54465 17.084 1.81147 15.3927 1.74533Z" fill="#FF0000"/>
+                 </svg>
+           </div>
+       </div>
+       
+   </div>`;
+   section.appendChild(newnode);
+       })
+       
+    }
